a553b4a5ea3d2f918d3631731a7045d326abbdd3
SAI PAVANI <111476294+MXR08230@users.noreply.github.com>
2022-09-01 18:23:52 -0500

    modified

--- question1.py
+++ question1.py
@@ -1,1 +1,37 @@
-print('manoj')
+import statistics
+
+ages=[19,22,19,24,20,25,26,24,25,24]
+print("ages_beforesort:", ages)
+#sorting the list
+sortedlist = ages.sort()
+#printing sorted list of ages
+print("sorted_ages:",ages)
+#printing minimum and maximum values 
+minimum=min(ages)
+maximum=max(ages)
+print("Minimum age is:", minimum)
+print("maximum age is:",maximum)
+
+#adding minimum and maximum values to list
+
+ages.append(minimum)
+ages.append(maximum)
+print("List of ages after appeending:",ages)
+
+#median
+
+median_age=statistics.median(ages)
+print("Median:", median_age)
+
+#average
+
+length=len(ages)
+s=0
+for i in ages:
+    s+=i
+print("Average:",s/length)
+
+#range
+
+ranges=maximum-minimum
+print("Range:",ranges)
---
+++ question2.py
@@ -0,0 +1,28 @@
+#creating empty dictionary called dog
+dog={}
+#Adding name, color, breed, legs, age to the dog dictionary
+dog={'name':'snoopy','color':'lavender','breed':'german_sheperd','legs':'4','age':'2'}
+print(dog)
+
+#creating student dictionary
+student={"first_name":"mounika","last_name":"Rayapudi","gender":"female","age":"21","martial status":"single",
+"skills":["java","c","python"],"country":"India","city":"Miryalaguda","address":"6708w apt 3001"}
+print(student)
+
+#length of student dictionary
+print("length of student:",len(student))
+
+#get values of skills and checking datatype
+print("student skills are:",student["skills"])
+print(type(student['skills']))
+
+
+#modifying skills by adding one or more
+student["skills"].append("javascript")
+print(student["skills"])
+
+#get dictionary keys as list
+print("student dictionary keys:",list(student.keys()))
+
+#get dictionary values as list
+print("student dictionary values:",list(student.values()))
